4f88675ab56946a4552d111347abc434b178e794
ops <odox700@gmail.com>
2020-09-04 12:33:30 -0400

    Do use-case: App sets page-number-navigator

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,7 +8,8 @@ use App\Http\Resources\ProductResource;
 
 class ProductController extends Controller
 {
-    public function featured() {
+    public function featured()
+    {
         return [
             'isResultOk' => true,
             'comment' => "CLASS: ProductController, METHOD: featured()",
@@ -18,11 +19,37 @@ class ProductController extends Controller
 
 
 
-    public function index() {
+    public function index(Request $request)
+    {
+
+        // 1)
+        $validatedData = $request->validate([
+            'page' => 'nullable|numeric',
+            'search' => 'nullable|string',
+        ]);
+
+
+        //
+        $numOfProductsPerPage = 9.0;
+        $numOfProducts = count(Product::all());
+        $numOfPages = $numOfProducts / $numOfProductsPerPage;
+        $roundedNumOfPages = ceil($numOfPages);
+
+        $paginationData = [
+            'numOfProducts' => $numOfProducts,
+            'numOfPages' => $numOfPages,
+            'roundedNumOfPages' => $roundedNumOfPages,
+            'currentPageNum' => $validatedData['page']
+        ];
+
+
+        //
         return [
             'isResultOk' => true,
             'comment' => "CLASS: ProductController, METHOD: index()",
-            'objs' => ProductResource::collection(Product::take(9)->get())
+            'objs' => ProductResource::collection(Product::take(9)->get()),
+            'paginationData' => $paginationData,
+            'validatedData' => $validatedData,
         ];
     }
 }
