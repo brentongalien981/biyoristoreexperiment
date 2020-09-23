7d7c3bd05b66b866ae83c01d9bdad2d9b2b2e241
ops <odox700@gmail.com>
2020-09-23 18:22:19 -0400

    finished use-case "USE-CASE005: user saves profile"
    finished use-case "USE-CASE005: user saves profile"

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,15 +11,35 @@ class ProfileController extends Controller
     public function save(Request $request)
     {
         //
+        $user = Auth::user();
+        $emailValidationCriteria = "email|min:8|max:64";
+        if ($user->email != $request->email) {
+            $emailValidationCriteria .= "|unique:users";
+        }
+
         $validatedData = $request->validate([
-            'email' => 'email|min:8|max:64|unique:users',
-            'phone' => 'nullable|numeric'
+            'email' => $emailValidationCriteria,
+            'firstName' => 'nullable|alpha|max:128',
+            'lastName' => 'nullable|alpha|max:128',
+            'phone' => 'nullable|string|max:16'
         ]);
+
+
+        //
+        $user->email = $validatedData['email'];
+        $user->save();
+
+        $profile = $user->profile;
+        $profile->first_name = isset($validatedData['firstName']) ? $validatedData['firstName'] : "";
+        $profile->last_name = isset($validatedData['lastName']) ? $validatedData['lastName'] : "";
+        $profile->phone = isset($validatedData['phone']) ? $validatedData['phone'] : "";
+        $profile->save();
 
 
         //
         return [
             'validatedData' => $validatedData,
+            'profile' => new ProfileResource($profile),
             'message' => "In CLASS: ProfileController, METHOD: save()"
         ];
     }
