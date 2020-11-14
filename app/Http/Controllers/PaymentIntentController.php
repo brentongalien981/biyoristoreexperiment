af3ccaf8b6585b17ddd8dd45183adacf9ebbcd94
ops <odox700@gmail.com>
2020-11-14 06:45:40 -0500

    still working on "USE-CASE007: user places order"
    still working on "USE-CASE007: user places order"

--- app/Http/Controllers/PaymentIntentController.php
+++ app/Http/Controllers/PaymentIntentController.php
@@ -3,27 +3,33 @@
 namespace App\Http\Controllers;
 
 use Error;
+use Exception;
 use Illuminate\Http\Request;
 
 class PaymentIntentController extends Controller
 {
     public function create()
     {
+
         // This is your real test secret API key.
         \Stripe\Stripe::setApiKey(env('STRIPE_SK'));
+
+
+        // TODO: Create order record with status "waiting-for-payment".
+
 
 
         try {
             $paymentIntent = \Stripe\PaymentIntent::create([
                 'amount' => 69,
-                'currency' => 'usd',
+                'currency' => 'cad',
             ]);
 
 
             return [
                 'clientSecret' => $paymentIntent->client_secret,
             ];
-        } catch (Error $e) {
+        } catch (Exception $e) {
             return ['customError' => $e->getMessage()];
         }
     }
