d3ff3bb1ab7c1d067406bc96df557c61c12cc3a6
brentongalien981 <brentongalien981@gmail.com>
2021-03-25 09:04:14 -0400

    for "UCG-03: user starts session", working on EXCEPTION TASKS
    for "UCG-03: user starts session", working on EXCEPTION TASKS

--- app/Http/Controllers/BmdAuthController.php
+++ app/Http/Controllers/BmdAuthController.php
@@ -12,7 +12,6 @@ class BmdAuthController extends Controller
 {
     public function trySalvageToken(Request $r)
     {
-        sleep(10);
         $bmdAuth = BmdAuthProvider::getInstance();
 
         if (
@@ -26,7 +25,6 @@ class BmdAuthController extends Controller
 
         return [
             'isResultOk' => true,
-            'bmdAuth' => $bmdAuth,
         ];
     }
 
@@ -53,47 +51,8 @@ class BmdAuthController extends Controller
 
     public function checkBmdAuthValidity(Request $r)
     {
-
-        // TODO
-        $bmdAuth = BmdAuthProvider::getInstance();
-        $isResultOk = false;
-        // $user = BmdAuthProvider::user();
-
-        //
-        $stayLoggedIn = $bmdAuth->stayLoggedIn;
-        if (
-            isset($stayLoggedIn)
-            && $stayLoggedIn == 1
-        ) {
-            $isResultOk = true;
-
-            $numOfOpenBrowserTabs = $bmdAuth->numOfOpenBrowserTabs;
-            if (!isset($numOfOpenBrowserTabs) || $numOfOpenBrowserTabs < 0) {
-                $numOfOpenBrowserTabs = 0;
-            }
-            $updatedNumOfOpenBrowserTabs = $numOfOpenBrowserTabs + 1;
-
-            $bmdAuth->numOfOpenBrowserTabs = $updatedNumOfOpenBrowserTabs;
-
-            // Update the bmd-auth cache-record only.
-            Cache::store('redisprimary')->put($bmdAuth->getCacheKey(), $bmdAuth);
-            // 1618997148
-
-
-        }
-
-        throw new Exception('muhaha');
-
         return [
-            'isResultOk' => $isResultOk,
-            'objs' => [
-                // 'email' => $user->email,
-                'bmdToken' => $bmdAuth->token,
-                'bmdRefreshToken' => $bmdAuth->refresh_token,
-                'frontendPseudoExpiresIn' => $bmdAuth->frontend_pseudo_expires_in,
-                'expiresIn' => $bmdAuth->expires_in,
-                'authProviderId' => $bmdAuth->auth_provider_type_id,
-            ],
+            'isResultOk' => true,
         ];
     }
 }
