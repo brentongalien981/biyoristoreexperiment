e36464930ebec100cbc8429e9e663381d4f169c1
odox700 <odox700@gmail.com>
2021-09-13 12:03:26 -0400

    for PROJ: MBMD / FT: Dispatch / UC-01: dispatch-manager creates dispatch, working on TASK: - handle event on-dispatch-save
    for PROJ: MBMD / FT: Dispatch / UC-01: dispatch-manager creates dispatch, working on TASK:
    - handle event on-dispatch-save

---
+++ database/seeds/DispatchStatusSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DispatchStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
