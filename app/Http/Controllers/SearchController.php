bf0262c8d903770ceed47e8f360086f00d63cb49
odox700 <odox700@gmail.com>
2021-10-16 15:06:49 -0400

    finishing UCG-02: app loads search-result-page
    finishing UCG-02: app loads search-result-page

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function index(Request $r)
+    {
+        $isResultOk = false;
+        $searchParams = [
+            'searchPhrase' => $r->searchPhrase,
+            'pageNum' => $r->pageNum
+        ];
+        $searchPageProducts = null;
+
+
+        try {
+            $searchPageProducts = Product::readSearchedProductsWithParams($searchParams);
+            $isResultOk = true;
+        } catch (\Throwable $th) {
+            
+        }
+
+
+        return [
+            'isResultOk' => $isResultOk,
+            'objs' => [
+                'searchPageProducts' => $searchPageProducts
+            ],
+            // BMD-DELETE
+            'requestData' => [
+                'searchPhrase' => $r->searchPhrase,
+                'pageNum' => $r->pageNum
+            ]
+        ];
+    }
+}
--- app/Product.php
+++ app/Product.php
@@ -16,6 +16,15 @@ use App\MyHelpers\Cache\CacheObjectsLifespanManager;
 
 class Product extends Model
 {
+    public const NUM_OF_PRODUCTS_PER_PAGE = 9;
+
+    public const SORT_BY_NAME_ASC = 1;
+    public const SORT_BY_NAME_DESC = 2;
+    public const SORT_BY_PRICE_ASC = 3;
+    public const SORT_BY_PRICE_DESC = 4;
+
+
+
     public static function getProductFromCache($productId, $retrieveJsonResource = false) {
 
         $cacheKey = ($retrieveJsonResource ? 'productResource' : 'product') . '?id=' . $productId;
@@ -87,5 +96,124 @@ class Product extends Model
     public function categories()
     {
         return $this->belongsToMany('App\Category', 'product_category');
+    }
+
+
+    
+    public static function readSearchedProductsWithParams($params = [])
+    {
+        $currentPageNum = $params['pageNum'] ?? 1;
+        $numOfSkippedItems = ($currentPageNum - 1) * self::NUM_OF_PRODUCTS_PER_PAGE;
+
+
+        $allQualifiedProductIds = [];
+        $searchPhraseQ = '%' . $params['searchPhrase'] . '%';
+        $qualifiedProductIds = Product::where('name', 'LIKE', $searchPhraseQ)->pluck('id')->toArray();
+        $allQualifiedProductIds = array_merge($allQualifiedProductIds, $qualifiedProductIds);
+
+        
+        $qualifiedTeams = Team::where('name', 'LIKE', $searchPhraseQ)->get();
+        foreach ($qualifiedTeams as $t) {
+            $qualifiedProductIds = $t->products()->whereNotIn('id', $allQualifiedProductIds)->pluck('id')->toArray();
+            $allQualifiedProductIds = array_merge($allQualifiedProductIds, $qualifiedProductIds);
+        }
+
+
+        $qualifiedCategories = Category::where('name', 'LIKE', $searchPhraseQ)->get();
+        foreach ($qualifiedCategories as $c) {
+            $qualifiedProductIds = $c->products()->whereNotIn('id', $allQualifiedProductIds)->pluck('id')->toArray();
+            $allQualifiedProductIds = array_merge($allQualifiedProductIds, $qualifiedProductIds);
+        }
+
+
+        $qualifiedBrands = Brand::where('name', 'LIKE', $searchPhraseQ)->get();
+        foreach ($qualifiedBrands as $b) {
+            $qualifiedProductIds = $b->products()->whereNotIn('id', $allQualifiedProductIds)->pluck('id')->toArray();
+            $allQualifiedProductIds = array_merge($allQualifiedProductIds, $qualifiedProductIds);
+        }
+
+
+        $numOfProductsForQuery = count($allQualifiedProductIds);
+        $products = Product::whereIn('id', $allQualifiedProductIds)->skip($numOfSkippedItems)->take(self::NUM_OF_PRODUCTS_PER_PAGE)->get();
+        $products = ProductResource::collection($products);
+
+        $numOfPages = $numOfProductsForQuery / (self::NUM_OF_PRODUCTS_PER_PAGE * 1.0);
+        $roundedNumOfPages = ceil($numOfPages);
+
+
+        return [
+            'products' => $products,
+            'paginationData' => [
+                'numOfProductsForQuery' => $numOfProductsForQuery,
+                'numOfPages' => $numOfPages,
+                'roundedNumOfPages' => $roundedNumOfPages,
+                'currentPageNum' => $currentPageNum,
+                'numOfSkippedItems' => $numOfSkippedItems
+            ]
+        ];
+    }
+
+
+
+    public static function readProductsWithParams($params = [])
+    {
+        $numOfProductsPerPage = self::NUM_OF_PRODUCTS_PER_PAGE;
+        $currentPageNum = isset($params['page']) ? $params['page'] : 1;
+        $numOfSkippedItems = ($currentPageNum - 1) * $numOfProductsPerPage;
+        $selectedBrandIds = isset($params['brands']) ? $params['brands'] : null;
+        $selectedTeamIds = isset($params['teams']) ? $params['teams'] : null;
+        $selectedCategoryId = isset($params['category']) ? $params['category'] : null;
+        $sortByCodeVal = isset($params['sort']) ? $params['sort'] : null;
+        $productsEloquentBuilder = self::where('id', '>', 0);
+        $products = [];
+        $numOfProductsForQuery = 0; // Number of all products for that query without restriction of the page number.
+
+
+
+        if (isset($selectedCategoryId)) {
+            $category = Category::find($selectedCategoryId);
+            $productsEloquentBuilder = $category->products();
+        }
+
+
+        if (isset($selectedBrandIds)) {
+            $productsEloquentBuilder = $productsEloquentBuilder->whereIn('brand_id', $selectedBrandIds);
+        }
+
+
+        if (isset($selectedTeamIds)) {
+            $productsEloquentBuilder = $productsEloquentBuilder->whereIn('team_id', $selectedTeamIds);
+        }
+
+
+        switch ($sortByCodeVal) {
+            case self::SORT_BY_NAME_DESC:
+                $productsEloquentBuilder = $productsEloquentBuilder->orderByDesc('name');
+                break;
+            default:
+                $productsEloquentBuilder = $productsEloquentBuilder->orderBy('name');
+                break;
+        }
+
+
+        $numOfProductsForQuery = count($productsEloquentBuilder->get());
+        $products = $productsEloquentBuilder->skip($numOfSkippedItems)->take($numOfProductsPerPage)->get();
+        $products = ProductResource::collection($products);
+
+
+        $numOfPages = $numOfProductsForQuery / ($numOfProductsPerPage * 1.0);
+        $roundedNumOfPages = ceil($numOfPages);
+
+
+        return [
+            'products' => $products,
+            'paginationData' => [
+                'numOfProductsForQuery' => $numOfProductsForQuery,
+                'numOfPages' => $numOfPages,
+                'roundedNumOfPages' => $roundedNumOfPages,
+                'currentPageNum' => $currentPageNum,
+                'numOfSkippedItems' => $numOfSkippedItems
+            ]
+        ];
     }
 }
